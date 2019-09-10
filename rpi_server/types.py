71b75513a5fefc56d96dcd332e5084062d5916b2
adwait.gautham@gmail.com <adwait.gautham@gmail.com>
2019-09-10 14:19:43 +0530

    Added types/trigger_request

---
+++ rpi_server/types.py
@@ -0,0 +1,15 @@
+"""
+All the request/response classes.
+"""
+
+class trigger_request(Object): 
+    
+    def __init__(self) :
+    
+        trigger_type = str()
+        machine = str()
+        return
+    
+    
+
+    
