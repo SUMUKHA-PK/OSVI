44e65ddc3126eac7088055197b574b43909a034d
adwait.gautham@gmail.com <adwait.gautham@gmail.com>
2019-11-04 06:30:09 +0530

    Working code!

--- rpi_server/relay.py
+++ rpi_server/relay.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python3
 
+
 from grovepi import *
-from time import sleep
+import RPi.GPIO as GPIO
 
-def relay(i, t) : 
-    assert pinMode(i, "OUTPUT") == 1
-    assert digitalWrite(i, 1) == 1
-    print("High")
-    sleep(t)
-    assert digitalWrite(i, 0) == 1
-    print("Low")
+"""
+Input - Output relay
+3 - 13
+4 - 12
+5 - 7
+6 - 26
+"""
 
-#if __name__ == "__main__":
-#    for i in range(2, 8):
-#        relay(i, 1)
+
+
+
+# Makes the relay input pin high, checks for existance of relay at the pin
+# First argument is the relay input
+# Second argument is the output of the relay
+def relayOn(inputR, outputR, t) : 
+    
+    print("Inside relayOn")
+
+    # triggering the relay
+    assert pinMode(inputR, "OUTPUT") == 1
+    assert digitalWrite(inputR, 1) == 1
+    print("Relay pin: %d made high"%(inputR))
+    
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(outputR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    # return the state of the output pin of the relay
+    return GPIO.input(outputR)
+
+# switches the relay off
+def relayOff(inputR,t):
+    assert pinMode(inputR,"OUTPUT") == 1
+    assert digitalWrite(inputR,0) == 1
+    print("Relay switched off")
