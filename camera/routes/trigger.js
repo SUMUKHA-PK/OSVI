ad6fcf73dbed2983146266992e0945dd6b4a4e16
26prajval98 <26prajval98@gmail.com>
2019-11-12 11:33:36 +0530

    node alternative for live stream capture

---
+++ camera/routes/trigger.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs')
+var http = require('http')
+var path = require('path')
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+	http.get('http://210.212.194.12:8888/feed1.webm', (r) => {
+		
+		var file = fs.createWriteStream( path.join(__dirname, '../public/videos/') + Number(new Date()).toString() + '.mp4')
+
+		var write = (x) => {
+			file.write(x)
+		}
+
+		r.on('data', write)
+
+		r.on('close', () => {
+			r.removeListener('data', write)
+			file.close()
+			file.end()
+		})
+
+		setTimeout(() => {
+			r.emit('close')
+			console.log("Done")
+			res.end()
+		}, 30000)
+	})
+});
+
+module.exports = router;
